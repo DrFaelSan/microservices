4ec8b9578700e6f791eeceff8388f3cd9663ba04
DrFaelSan <rafaelvplima@gmail.com>
2024-05-05 11:50:46 -0300

    feat[BE] - Configurando no novo notebook

--- DevShooping/DevShooping.ProductAPI/obj/Debug/net6.0/DevShooping.ProductAPI.AssemblyInfo.cs
+++ DevShooping/DevShooping.ProductAPI/obj/Debug/net6.0/DevShooping.ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevShooping.ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b022daa6e8f59c5e9e482ed149edf0750c8acceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevShooping.ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevShooping.ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
